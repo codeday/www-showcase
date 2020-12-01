47b5e6cf51b0e990ea631dff30e251c273cf85f3
Tyler Menezes <tylermenezes@codeday.org>
2020-11-30 19:48:04 -0500

    Add bad-looking previews in gallery views

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Box from '@codeday/topo/Atom/Box';
 import Text, { Heading } from '@codeday/topo/Atom/Text';
 import { PROJECT_TYPES } from '../util/projectTypes';
+import Image from '@codeday/topo/Atom/Image';
 
 export default function ProjectPreview({ project }) {
   return (
@@ -14,6 +15,9 @@ export default function ProjectPreview({ project }) {
       rounded="md"
     >
       <Box p={4}>
+        {project.media?.length > 0 && (
+          <Image src={project.media[0].image} alt="" />
+        )}
         <Heading as="h3" fontSize="xl">{project.name}</Heading>
         <Text mb={0}>{PROJECT_TYPES[project.type]}</Text>
       </Box>
