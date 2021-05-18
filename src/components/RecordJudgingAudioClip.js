e7db84ca45480bf7c690bad154a471f5161d08f6
Lola <lola@egherman.com>
2021-05-17 19:03:00 -0500

    add audio clip support

---
+++ src/components/RecordJudgingAudioClip.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { ReactMediaRecorder } from 'react-media-recorder';
+import { Heading } from '@codeday/topo/Atom/Text';
+import Box from '@codeday/topo/Atom/Box';
+import Button from '@codeday/topo/Atom/Button';
+import H5AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+let statusHeading;
+export default function RecordJudgingAudioClip({ ...props }) {
+  return (
+    <ReactMediaRecorder
+      render={({
+        status,
+        startRecording,
+        stopRecording,
+        mediaBlobUrl,
+        clearBlobUrl,
+      }) => {
+        switch (status) {
+          case 'recording':
+            statusHeading = <Heading m={4} size="lg">🔴 Recording...</Heading>;
+            break;
+          case 'idle':
+            statusHeading = <Heading m={4} size="lg">✅ Ready to record!</Heading>;
+            break;
+          case 'stopped':
+            statusHeading = (mediaBlobUrl)
+              ? <Heading m={4} size="lg">👀 Review your audio:</Heading>
+              : <Heading m={4} size="lg">✅ Ready to record!</Heading>;
+            break;
+          default:
+            break;
+        }
+        return (
+          <Box textAlign="center" {...props}>
+            {statusHeading}
+            <H5AudioPlayer
+              src={(mediaBlobUrl) ? mediaBlobUrl : ''}
+              autoPlay
+              showJumpControls={false}
+              customAdditionalControls={[]}
+            />
+            <Button m={4} onClick={startRecording} disabled={(status === 'recording')} variantColor="green">
+              Start Recording
+            </Button>
+            <Button m={4} onClick={stopRecording} disabled={(status !== 'recording')} variantColor="red">
+              Stop Recording
+            </Button>
+            {(mediaBlobUrl)
+              ? (
+                <Box>
+                  <Button m={4} onClick={clearBlobUrl} variantColor="red">✖ Retry</Button>
+                  {/* todo post file contents to /api/uploadJudgeComments */}
+                  <Button m={4} variantColor="green">✔ Upload</Button>
+                </Box>
+              )
+              : null}
+          </Box>
+        );
+      }}
+    />
+  );
+}
--- src/pages/judge/record/[project]/index.js
+++ src/pages/judge/record/[project]/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Button from '@codeday/topo/Atom/Button';
 import Content from '@codeday/topo/Molecule/Content';
 import RecordJudgingVideoClip from '../../../../components/RecordJudgingVideoClip';
+import RecordJudgingAudioClip from '../../../../components/RecordJudgingAudioClip';
 import Page from '../../../../components/Page';
 
 export default function JudgingRecord() {
   return (
     <Page>
       <Content>
-        <RecordJudgingVideoClip />
+        <RecordJudgingAudioClip />
       </Content>
     </Page>
   );
