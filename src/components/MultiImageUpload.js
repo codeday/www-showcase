6d30bb433a104d27f8591c731b10b8b844ef2456
Tyler Menezes <tylermenezes@codeday.org>
2022-08-22 16:50:04 -0700

    Add file uploads

---
+++ src/components/MultiImageUpload.js
@@ -0,0 +1,122 @@
+import Box from '@codeday/topo/Atom/Box';
+import Button from '@codeday/topo/Atom/Button';
+import List, { Item as ListItem } from '@codeday/topo/Atom/List';
+import { Stack } from '@chakra-ui/core';
+import { useState } from 'react';
+import Files from 'react-butterfiles';
+import { useList } from 'react-use';
+import Text from '@codeday/topo/Atom/Text';
+
+const STATUS_COLORS = {
+  pending: 'blue.500',
+  failed: 'red.500',
+  succeeded: 'green.500',
+}
+
+export default function MultiImageUpload({ uploadPhoto, ...props }) {
+  const [errors, { push: pushErrors }] = useList([]);
+  const [dragging, setDragging] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [files, { push, removeAt, updateAt }] = useList([]);
+
+  return (
+    <Box borderColor={dragging ? 'blue.500' : undefined} borderWidth={2} p={8} {...props}>
+      <Files
+          multiple
+          convertToBase64
+          accept={["image/jpg", "image/jpeg", "image/png"]}
+          onError={(e) => pushErrors(...e)}
+          onSuccess={f => push(...f)}
+      >
+          {({ browseFiles, getDropZoneProps }) => (
+              <Box
+                  {...getDropZoneProps({
+                      onDragEnter: () => setDragging(true),
+                      onDragLeave: () => setDragging(false),
+                      onDrop: () => setDragging(false),
+                  })}
+              >
+                  <Stack direction="row" spacing={2}>
+                      {files.map((image, index) => (
+                          <Box
+                              key={index}
+                              onClick={() => removeAt(index)}
+                              cursor="pointer"
+                              borderWidth={3}
+                              borderColor={STATUS_COLORS[image.status]}
+                          >
+                            <Box
+                              h={24}
+                              w={24}
+                              backgroundImage={`url(${image.src.base64})`}
+                              backgroundPosition="50% 50%"
+                              backgroundSize="cover"
+                              opacity={image.status && image.status !== 'pending' ? 0.5 : 1}
+                            />
+                          </Box>
+                      ))}
+                      <Box
+                        cursor="pointer"
+                        borderWidth={3}
+                        borderColor="gray.100"
+                        onClick={() => {
+                          browseFiles({
+                            onErrors: (e) => pushErrors(...e),
+                            onSuccess: (f) => push(...f),
+                          });
+                        }}
+                      >
+                        <Box
+                          h={24}
+                          w={24}
+                          textAlign="center"
+                          pt={8}
+                          fontSize="2xl"
+                          backgroundColor="gray.100"
+                        >
+                          +
+                        </Box>
+                      </Box>
+                  </Stack>
+              </Box>
+          )}
+      </Files>
+      <Button
+        mt={8}
+        w="100%"
+        disabled={files.length === 0}
+        isLoading={isLoading}
+        onClick={async () => {
+          setIsLoading(true);
+          for (const i in files) {
+            const file = files[i];
+            if (file.status) continue;
+
+            updateAt(i, { ...file, status: 'pending' });
+            try {
+              await uploadPhoto(file);
+              updateAt(i, { ...file, status: 'succeeded' });
+            }
+            catch (ex) {
+              pushErrors({ file, message: ex.toString()});
+              updateAt(i, { ...file, status: 'failed' });
+            }
+          }
+          setIsLoading(false);
+        }}
+      >
+        Upload All
+      </Button>
+      {errors.length > 0 && (
+        <Box color="red.600" mt={8}>
+          <Text mb={0} fontWeight="bold">Errors:</Text>
+          <List styleType="disc">
+            {errors.map((e, i) => (
+              <ListItem key={i}>{e.file?.name}: {e.message || e.type}</ListItem>
+            ))}
+          </List>
+        </Box>
+      )}
+    </Box>
+  );
+}
---
+++ src/pages/upload-photos.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/client';
+import Content from '@codeday/topo/Molecule/Content';
+import { Heading, Link } from '@codeday/topo/Atom/Text';
+import List, { Item as ListItem } from '@codeday/topo/Atom/List';
+import Page from '../components/Page';
+import ForceLoginPage from '../components/ForceLoginPage';
+import MultiImageUpload from '../components/MultiImageUpload';
+import { GetAllEventsQuery, UploadPhotoMutation } from './upload-photos.gql';
+import { apiFetch } from '@codeday/topo/utils';
+import { default as Input } from '@codeday/topo/Atom/Input/Text';
+import { tryAuthenticatedApiQuery } from '../util/api';
+import { mintToken } from '../util/token';
+
+export default function UploadPhotos({ logIn, allEvents, token }) {
+  const [thanks, setThanks] = useState('');
+  const [event, setEvent] = useState(null);
+
+  const eventInfo = allEvents?.cms?.events?.items
+    ?.flatMap(eg => Object.entries(eg.subEventIds).map(([eventId, nameOrConfig]) => ({
+      programId: eg.program?.webname,
+      eventGroupId: eg.id,
+      eventId,
+      region: typeof nameOrConfig === 'string' ? undefined : nameOrConfig.region,
+      title: typeof nameOrConfig === 'string' ? nameOrConfig : nameOrConfig.title,
+    })));
+
+  if (logIn) return <ForceLoginPage />;
+
+  if (!event) {
+    return (
+      <Page slug="/upload-photos">
+        <Content mt={-8}>
+          <Heading as="h2" fontSize="4xl">Upload Photos</Heading>
+          <Heading as="h3" fontSize="2xl">1. Enter Photo Credit (Optional)</Heading>
+          <Input value={thanks} onChange={(e) => setThanks(e.target.value)} placeholder="(e.g. photographer name)" />
+
+          <Heading as="h3" fontSize="2xl" mt={8}>2. Select An Event</Heading>
+          <List styleType="disc">
+            {eventInfo.map((ei) => (
+              <ListItem key={ei.eventId}>
+                <Link
+                  onClick={() => setEvent(ei)}
+                >{ei.eventGroupId} ~ {ei.title}</Link>
+              </ListItem>
+            ))}
+          </List>
+        </Content>
+      </Page>
+    )
+  }
+
+  return (
+    <Page slug="/upload-photos">
+      <Content mt={-8}>
+        <Heading as="h2" fontSize="4xl" mb={8}>Upload Photos: {event.eventGroupId} ~ {event.title}</Heading>
+        <MultiImageUpload
+          uploadPhoto={async (file) => {
+            const { result, error: resultError } = await tryAuthenticatedApiQuery(
+              UploadPhotoMutation,
+              { upload: file.src.file, ...event, thanks },
+              token
+            );
+            if (resultError) throw new Error(resultError);
+          }}
+        />
+      </Content>
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ req, params }) {
+  const session = await getSession({ req });
+  if (!session?.user?.admin) return { props: { logIn: true } };
+
+  const allEvents = await apiFetch(GetAllEventsQuery);
+  const token = mintToken(session);
+
+  return { props: {
+    allEvents,
+    token,
+  } };
+}
