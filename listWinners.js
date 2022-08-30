5eae67726882081827e85adb1aabaa131b85d954
Tyler Menezes <x@tyler.vc>
2022-08-29 17:35:50 -0700

    Update listWinners to fetch email

--- listWinners.js
+++ listWinners.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const { sign } = require('jsonwebtoken');
 const { apiFetch } = require('@codeday/topo/utils');
 
-const token = sign({ scopes: 'write:users' }, process.env.ACCOUNT_SECRET, { expiresIn: '5m' });
+const token = sign({ scopes: 'read:users' }, process.env.ACCOUNT_SECRET, { expiresIn: '5m' });
 
 // eslint-disable-next-line no-secrets/no-secrets
 const queryFetchMembers = `
@@ -21,6 +21,7 @@ const queryFetchMembers = `
           account {
             name
             discordId
+            email
           }
         }
       }
@@ -29,11 +30,12 @@ const queryFetchMembers = `
 `;
 
 async function fetchUsers(season) {
-  const result = await apiFetch(queryFetchMembers, { eventGroup: season });
+  const result = await apiFetch(queryFetchMembers, { eventGroup: season }, { Authorization: `Bearer ${token}` });
   return result.showcase
     .projects.map((p) => p.members.map((m) => p.awards.map((a) => ({
       name: m.account.name.replace(/,/g, ''),
       discordId: m.account.discordId,
+      email: m.account.email,
       projectName: p.name.replace(/,/g, ''),
       award: a.modifier ? `${a.type} - ${a.modifier}` : a.type,
     }))))
@@ -43,8 +45,8 @@ async function fetchUsers(season) {
 
 (async () => {
   const users = await fetchUsers(process.argv[2]);
-  console.log(`name,discordTag,project,award`);
+  console.log(`name,email,discordTag,project,award`);
   for (const user of users) {
-    console.log(`${user.name},<@${user.discordId}>,${user.projectName},${user.award}`);
+    console.log(`${user.name},${user.email},<@${user.discordId}>,${user.projectName},${user.award}`);
   }
 })();
