8ab2c9c90af31299aaa1c695816211b8936e2638
Lola <lola@egherman.com>
2023-03-16 17:09:15 -0500

    facepalm, no wonder the site wasn't working

--- src/util/api.js
+++ src/util/api.js
@@ -1,11 +1,6 @@
 import { print } from 'graphql';
-import { GraphQLClient } from 'graphql-request';
-// import { apiFetch } from '@codeday/topo/utils';
+import { apiFetch } from '@codeday/topo/utils';
 
-const apiFetch = (query, variables, headers) => {
-  const client = new GraphQLClient('http://localhost:4000', { headers });
-  return client.request(query, variables);
-};
 export async function tryAuthenticatedApiQuery(gql, params, token) {
   const headers = {
     'X-Showcase-Authorization': `Bearer ${token}`,
